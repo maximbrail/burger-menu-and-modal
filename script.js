7c441c013cf21c54bde493a323e984a39d3da32c
maximbrail <maxabobus953@gmail.com>
2024-09-05 20:49:39 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-const menu = document.querySelector(".menu")
+const links = document.querySelector(".links")
 const burger = document.querySelector(".burger")
 
 burger.onclick = () => {
-    menu.classList.toggle('open')
-}
-
-
-const btnOpen = document.querySelector(".btn")
-const btnClose = document.querySelector(".close-btn")
-const modal = document.querySelector(".modal") 
-
-btnOpen.onclick = () =>{
-    modal.classList.add("open-modal")
-}
-
-btnClose.onclick = () =>{
-    modal.classList.remove("open-modal")
+    links.classList.toggle('open')
 }
